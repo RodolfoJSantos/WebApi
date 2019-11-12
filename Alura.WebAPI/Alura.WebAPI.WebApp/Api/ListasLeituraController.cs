141c754313a7815aff78e961dd469e5d131dd446
rodolfo santos <rodjsant@outlook.com>
2019-11-12 00:34:07 -0300

    home

---
+++ Alura.WebAPI/Alura.WebAPI.WebApp/Api/ListasLeituraController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Alura.ListaLeitura.Modelos;
+using Alura.ListaLeitura.Persistencia;
+using Microsoft.AspNetCore.Mvc;
+using Lista = Alura.ListaLeitura.Modelos.ListaLeitura;
+
+namespace Alura.WebAPI.WebApp.Api
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ListasLeituraController : Controller
+    {
+        private readonly IRepository<Livro> _repo;
+
+        public ListasLeituraController(IRepository<Livro> repository)
+        {
+            _repo = repository;
+        }
+
+        private Lista CriaLista(TipoListaLeitura tipo)
+        {
+            return new Lista
+            {
+                Tipo = tipo.ParaString(),
+                Livros = _repo.All.Where(l => l.Lista == tipo).ToList()
+            };
+        }
+
+        [HttpGet]
+        public IActionResult TodasLista()
+        {
+            Lista paraLer = CriaLista(TipoListaLeitura.ParaLer);
+            Lista lendo = CriaLista(TipoListaLeitura.Lendo);
+            Lista lidos = CriaLista(TipoListaLeitura.Lidos);
+
+            var colecao = new List<Lista> { paraLer, lendo, lidos };
+            return Ok(colecao);
+        }
+
+        [HttpGet]
+        [Route("{tipo}")]
+        public IActionResult Recuperar(TipoListaLeitura tipo)
+        {
+            var lista = _repo.All.Where(l => l.Lista == tipo);
+            return Ok(lista);
+        }
+    }
+}
